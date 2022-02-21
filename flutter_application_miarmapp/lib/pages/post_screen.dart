d16fba0234d8df110e5f2901a0d646174dae2130
vicente1991 <etenciv@gmail.com>
2022-02-21 21:42:05 +0100

    prueba 1

--- flutter_application_miarmapp/lib/pages/post_screen.dart
+++ flutter_application_miarmapp/lib/pages/post_screen.dart
@@ -38,6 +38,7 @@ class _PostScreenState extends State<PostScreen> {
                )
               ),
             ),
+            buildActions(),
           ],
         ),
         onDoubleTap: (){
