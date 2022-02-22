35963e5e6d64c8116ac7847f5b709cfbaf2d29cd
vicente <etenciv@gmail.com>
2022-02-22 13:46:51 +0100

    token

--- flutter_application_miarmapp/lib/repository/post_repository/post_respository_impl.dart
+++ flutter_application_miarmapp/lib/repository/post_repository/post_respository_impl.dart
@@ -7,19 +7,22 @@ import 'package:http/http.dart';
 class PostRepositoryImpl extends PostRepository {
   final Client _client = Client();
 
-
   /*myModels=(json.decode(response.body) as List).map((i) =>
               MyModel.fromJson(i)).toList();*/
 
   @override
   Future<List<PostResponse>> fetchPost(String type) async {
-    final response = await _client.get(Uri.parse('http://10.0.2.2:8080/post/public'));
+    final response = await _client
+        .get(Uri.parse('http://10.0.2.2:8080/post/public'), headers: {
+      'Authorization':
+          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYzFiNDYwZC03ZjIxLTFjYjMtODE3Zi0yMTNkMzFiYTAwMDAiLCJpYXQiOjE2NDU2MTY1NDYsIm5vbWJyZSI6IlZpY2VudGUifQ.7wd738QZ_jbyrwaO6hPwLaI-0vQ2ZZRAlgIfsGdBy6c',
+    });
     if (response.statusCode == 200) {
-      return (json.decode(response.body) as List).map((i) =>
-              PostResponse.fromJson(i)).toList();
+      return (json.decode(response.body) as List)
+          .map((i) => PostResponse.fromJson(i))
+          .toList();
     } else {
       throw Exception('Fail to load post');
     }
   }
-
 }
